3f76faf254326425414c2f2a4ea5366539bf2249
AlexViolette <alex.violettee@gmail.com>
2026-04-10 15:00:11 +0200

    [FEAT] Mise en place de 3 container (Symfony, React, MySQL)

---
+++ Symfony/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+until php bin/console doctrine:query:sql "SELECT 1" >/dev/null 2>&1; do
+	sleep 2
+done
+
+php bin/console doctrine:migrations:sync-metadata-storage --no-interaction
+php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
+
+exec apache2-foreground
